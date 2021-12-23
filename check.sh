ddb03b9e544da976193be8fd07a8a23876d54576
Yusuf Papurcu <yusufturhanp@gmail.com>
2021-12-23 10:06:33 +0300

    dev(shellscript): Shell script improvements
    License check problem fixed
    go-licenses stdError output captured for fix this problem

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
-if [[ $(go-licenses check .) ]]; then
+#!/bin/bash 
+output=$(go-licenses check . 2>&1)
+if [ -z "$output" ]; then
     echo "License Check Success"
+    exit 0
 else
     echo "License Check Failed"
     exit 1
